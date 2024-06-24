30838bdb34496dbf30f960b99ea09a987f5d8686
KadHau7 <Kadeemtheengineer@gmail.com>
2024-06-23 20:47:03 -0500

    Add files via upload

---
+++ Github prototype 1 - Dune/# Kwisatz Haderach.py
@@ -0,0 +1,69 @@
+# kwisatz haderach
+import random
+print("I AM PAUL MAUD'DIB ATREIDES, DUKE OF ARRAKIS!!") #program greeting
+name = input("what is your name fremen >>> ") # Asking user for input of their name
+print("Hello", name, "there are many possible futures but I can see it") #Engaging and acknowledging the user
+question = input("what is the question you want to ask >>> ") #asking user for a question via an input
+answer = "" # answer is equal to an empty string
+random_number = random.randint(1,25) #asking the program to generate a random number from 1 to 25
+if random_number == 1: #using an if, elif, else statements to quote the line from the movie's protagonist Paul Atreides
+    print(name, "he who can destroy a thing, controls a thing.")
+elif random_number == 2:
+    print(name, "all paths lead into darkness.")
+elif random_number == 3:
+    print(name, "the eye that looks ahead to the safe course is closed forever.")
+elif random_number == 4:
+    print(name, "they'll come to understand, I've seen it")
+elif random_number == 5:
+    print(name, "each man is a little war.")
+elif random_number == 6:
+    print(name, "repeat after me,  I must not fear. Fear is the mind-killer. Fear is the little death that brings obliteration. I will face my fear and I will permit it to pass over me and through me. And when it has gone past... I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.")
+elif random_number == 7:
+    print(name, "your road leads into the desert")
+elif random_number == 8:
+    print(name, "the visions are clear now. I see possible futures, all at once. Your enemies are all around you, and in so many futures they prevail. But I do see a way, there is a narrow way through.")
+elif random_number == 9:
+    print(name, "prepare for battle. May thy knife chip and shatter.")
+elif random_number == 10:
+    print(name, "remember those around you, they will love you as long as they breath.")
+elif random_number == 11:
+    print(name, "my mother once told me how seductive it is to live in peace.")
+elif random_number == 12:
+    print(name, "my mother told me anything outside yourself, this you can see and apply your logic to it. But it's a human trait that when we encounter personal problems, these things most deeply personal are the most difficult to bring out for our logic to scan. We tend to flounder around, blaming everything but the actual, deep-seated thing that's really chewing on us.")
+elif random_number == 13:
+    print(name, "my mother said motivating people, forcing them to your will, gives you a cynical attitude towards humanity. It degrades everything it touches.")
+elif random_number == 14:
+    print(name, "my father once told me a person needs new experiences. It jars something deep inside, allowing them to grow. Without change something sleeps inside us, and seldom awakens. The sleeper must awaken.")
+elif random_number == 15:
+    print(name, "my father said once you have explored a fear, it becomes less terrifying. Part of courage comes from extending our knowledge.")
+elif random_number == 16:
+    print(name, "respect for the truth is the basis for all morality. Something cannot emerge from nothing")
+elif random_number == 17:
+    print(name, "it is human to have your soul brought to a crisis you did not anticipate.")
+elif random_number == 18:
+    print(name, "Gurney said The slow blade penetrates the shield.")
+elif random_number == 19:
+    print(name, "in the vision the Bene Gesserit said survival is the ability to swim in strange water.")
+elif random_number == 20:
+    print(name, "there should be a science of discontent. People need hard times to develop psychic muscles.")
+elif random_number == 21:
+    print(name, "The ancient Fremen have a saying, truth suffers from too much analysis")
+elif random_number == 22:
+    print(name, "Princes Irulan says you underestimate the power of faith")
+elif random_number == 23:
+    print(name, "You cannot see the future without seeing the past.")
+elif random_number == 24:
+    print(name, "Our resources are limited, fear is all we have")
+elif random_number == 25:
+    print(name, "Long live the fighters!!!")
+else:
+    print("ERROR")
+
+print(answer)
+print("Until next time!")
+
+
+
+
+  
+
